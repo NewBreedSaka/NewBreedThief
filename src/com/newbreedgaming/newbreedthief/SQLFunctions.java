52bfbe25de948f93c22ba4c90c5a2bd7e0f017a3
EquinoxDevNew <workwestinmiller@gmail.com>
2014-06-01 11:06:31 -0700

    Latest
    Latest

--- src/com/newbreedgaming/newbreedthief/SQLFunctions.java
+++ src/com/newbreedgaming/newbreedthief/SQLFunctions.java
@@ -13,10 +13,10 @@ public class SQLFunctions {
 	Main plugin;
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://http://76.74.200.77/playerdata";
+	static final String DB_URL = "jdbc:mysql://76.74.200.77/playerdata";
 	//  Database credentials
-	static final String USER = "root";
-	static final String PASS = "w6m5Ox90Sv";
+	static final String USER = "newbreed";
+	static final String PASS = "newbreedservertesting";
 	Statement stmt;
 	Connection conn;
 	public SQLFunctions(Main plugin){
@@ -34,7 +34,7 @@ public class SQLFunctions {
 			stmt = conn.createStatement();
 			String sql = "CREATE TABLE " +  p.getName() + "( Kills int, Deaths int, TPoints int , UTPoints int, Wins int, Losses int, UUID varchar(255))"; 
 			String asql = "INSERT INTO " + p.getName() + "(UUID) VALUES ('"+ p.getUniqueId() +"');";
-			stmt.executeUpdate(asql);
+			
 			stmt.executeUpdate(sql);
 		}catch(SQLException se){
 			//Handle errors for JDBC
